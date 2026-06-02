da9c5dd7fe7f8d79be91737e398f00b4e72a79c0
SeeRaze <zhekanizkodubov@mail.ru>
2026-06-03 00:12:08 +0300

    Update __init__.py

--- core/cards/buff/__init__.py
+++ core/cards/buff/__init__.py
@@ -1,2 +1,3 @@
 from core.cards.buff.strength import create_flex, create_battle_cry
 from core.cards.buff.thorns import create_thorn_armor
+from core.cards.buff.regen import create_regenerate, create_vitality, create_triage
